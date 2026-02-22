25ae75da2300bfdc929fdc2d36044adc851fd3c3
Arya Sen <aryavolkan@gmail.com>
2026-02-22 13:18:06 -0800

    fix lint

--- godot/register_physics.gd
+++ godot/register_physics.gd
@@ -1,14 +1,14 @@
 extends Node
 
 func _enter_tree() -> void:
-	if ClassDB.class_exists(&"EvolvePhysicsServer"):
-		PhysicsServer2DManager.register_server(
-			"EvolvePhysics",
-			Callable(self, "_create_server")
-		)
-		print("[EvolvePhysics] ✓ Rapier2D physics server registered")
-	else:
-		print("[EvolvePhysics] ⚠ Extension not loaded")
+    if ClassDB.class_exists(&"EvolvePhysicsServer"):
+        PhysicsServer2DManager.register_server(
+            "EvolvePhysics",
+            Callable(self, "_create_server")
+        )
+        print("[EvolvePhysics] ✓ Rapier2D physics server registered")
+    else:
+        print("[EvolvePhysics] ⚠ Extension not loaded")
 
 func _create_server() -> PhysicsServer2D:
-	return EvolvePhysicsServer.new()
+    return EvolvePhysicsServer.new()
